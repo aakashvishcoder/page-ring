7a95183684c354d431f319114a5f54f0e7efb75e
heygideon <47083178+heygideon@users.noreply.github.com>
2025-12-20 21:34:34 +0000

    fix: prod webring api url

--- packages/embed/src/lib/consts.ts
+++ packages/embed/src/lib/consts.ts
@@ -1,6 +1,8 @@
 import { joinURL } from "ufo";
 
-export const RING_BASE = import.meta.env.PROD ? "" : "http://localhost:4321/";
+export const RING_BASE = import.meta.env.PROD
+  ? "https://pagering.gideon.sh/"
+  : "http://localhost:4321/";
 export const API_BASE = joinURL(RING_BASE, "/api/v1");
 
 export const flower = joinURL(RING_BASE, "/flower.png");
